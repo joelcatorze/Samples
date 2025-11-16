d60144d01b26dece795491ee6745ba1d92c19209
joelcatorze <92091039+joelcatorze@users.noreply.github.com>
2025-11-16 14:52:15 +0800

    Add files via upload

---
+++ Active Listening Skill Assessment/story_content/triggers.js
@@ -0,0 +1,137 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5p1frrUQjpD":
+        Script1();
+        break;
+      case "6CtVaweFIMJ":
+        Script2();
+        break;
+      case "5niQCZovu1W":
+        Script3();
+        break;
+      case "5y4cGDWBeY2":
+        Script4();
+        break;
+      case "5X72Y8kSBN6":
+        Script5();
+        break;
+      case "5ikCnXVfHlj":
+        Script6();
+        break;
+      case "5hroioTNiF6":
+        Script7();
+        break;
+      case "6DH4iyMUcZy":
+        Script8();
+        break;
+      case "5ewY5uBc52G":
+        Script9();
+        break;
+      case "5kZpyOTqXFG":
+        Script10();
+        break;
+      case "6jL1scPnxUT":
+        Script11();
+        break;
+      case "5qcrheKN7Bz":
+        Script12();
+        break;
+      case "5lAayW54Wej":
+        Script13();
+        break;
+      case "6ELcph205zk":
+        Script14();
+        break;
+      case "5q1c78VrL1g":
+        Script15();
+        break;
+      case "5doWIdtV19E":
+        Script16();
+        break;
+      case "5op9XL1okIu":
+        Script17();
+        break;
+      case "5V4VGciWdp4":
+        Script18();
+        break;
+      case "61fVE1MBGt5":
+        Script19();
+        break;
+      case "5aRIsMxWK6o":
+        Script20();
+        break;
+      case "6QIu4ifBTHa":
+        Script21();
+        break;
+      case "5XYvN1peRPp":
+        Script22();
+        break;
+      case "6Yp9tLLrXG1":
+        Script23();
+        break;
+      case "63vsyi79IXt":
+        Script24();
+        break;
+      case "68ec2IFVSwt":
+        Script25();
+        break;
+      case "6Djznrg2tCe":
+        Script26();
+        break;
+      case "5iFrD5X1nRp":
+        Script27();
+        break;
+      case "5byd3Xrt98O":
+        Script28();
+        break;
+      case "5YsuaRkjW3x":
+        Script29();
+        break;
+      case "6rUT9k4IbEK":
+        Script30();
+        break;
+      case "5zJ98s6U9YG":
+        Script31();
+        break;
+      case "6J2vUKbhx8y":
+        Script32();
+        break;
+      case "6ficWa8hOf6":
+        Script33();
+        break;
+      case "6MwXZXoetwt":
+        Script34();
+        break;
+      case "5sWHmcVorsI":
+        Script35();
+        break;
+      case "6cJwBoIRROg":
+        Script36();
+        break;
+      case "6WJ0mbepX2F":
+        Script37();
+        break;
+      case "64qimYHBlOY":
+        Script38();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
